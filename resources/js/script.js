b60893b87cf24739fd6d32cf970a98f517f6c842
Igor <puzo88@gmail.com>
2016-11-22 19:57:17 +0100

    add sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	
+	$('.js--section-about').waypoint(function(direction) {
+      if(direction =="down") {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    }, {
+      offset: '60px'
+    });
 
 	/* Map */
 	var map = new GMaps({
